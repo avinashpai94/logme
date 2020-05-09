e06f759c0cbd5c1556b36764896a5617b9876c3a
athirana <anambiat@asu.edu>
2020-05-08 23:15:00 -0700

    event_source
    Code for event source added.
    Installations are in requirements.txt

--- cloudsql/main.py
+++ cloudsql/main.py
@@ -14,140 +14,146 @@
 
 # [START gae_python37_cloudsql_mysql]
 import os
+import hashlib
+#from firebase import Firebase
+from flask import Flask,jsonify,request,Response
+from firebase import Firebase
+from datetime import datetime
+import time
+import json
 
-from flask import Flask,jsonify,request
-import pymysql
+config = {
+  "apiKey": "AIzaSyA4UPj1_g5KDqMPTkVPqF4Kh7S1dnIgU0Q",
+  "authDomain": "errorlogger-c8536.firebaseapp.com",
+  "databaseURL": "https://errorlogger-c8536.firebaseio.com",
+  "storageBucket": "errorlogger.appspot.com",
+  "serviceAccount": "./firebase_key/errorlogger-d5dc9028e1af.json"
+}
 
-db_user = os.environ.get('CLOUD_SQL_USERNAME')
-db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
-db_user="root"
-db_password="root"
-db_name="clouddb"
-db_connection_name="errorlogger:us-west4:loggerdb"
 
 app = Flask(__name__)
+firebase = Firebase(config)
+db = firebase.database()
 
 
 @app.route('/')
 def homepage():
-    return(jsonify("hi"))
+    return(jsonify("Welcome"))
 
-@app.route('/history')
-def getHistory():
+@app.route('/add_device',methods=['GET'])
+def getkey():
     # When deployed to App Engine, the `GAE_ENV` environment variable will be
     # set to `standard`
-    if os.environ.get('GAE_ENV') == 'standard':
-        # If deployed, use the local socket interface for accessing Cloud SQL
-        unix_socket = '/cloudsql/{}'.format(db_connection_name)
-        cnx = pymysql.connect(user=db_user, password=db_password,
-                              unix_socket=unix_socket, db=db_name)
+
+    email = request.args.get('email')
+    device= request.args.get('device')
+
+    email_obj = hashlib.md5(email.encode())
+    email_hex=email_obj.hexdigest()
+
+
+    timestamp=datetime.utcnow()
+    email_time=str(email)+str(timestamp)
+    access_obj = hashlib.md5(email_time.encode())
+    access_hex=access_obj.hexdigest()
+
+
+    if not db.child('users').child(email_hex).shallow().get().val(): # if email doesn't exist
+
+       #create email and access token
+        db.child('users').update({email_hex:
+                {
+                   access_hex:0
+                }
+
+        })
     else:
-        # If running locally, use the TCP connections instead
-        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
-        # so that your application can use 127.0.0.1:3306 to connect to your
-        # Cloud SQL instance
-        host = '127.0.0.1'
-        host="34.125.50.185"
-        cnx = pymysql.connect(user=db_user, password=db_password,
-                              host=host, db=db_name)
-    accesstokens = request.args.get('accesstokens')
-    number = request.args.get('n')
-    # print(username)
-    # print("num",number)
-    with cnx.cursor() as cursor:
+        #create access token
+        db.child('users').child(email_hex).update(
+                   {
+                      access_hex:0
+                   }
 
-        num=int(number)
-        print(type(num))
-        query="SELECT * FROM Notifications where accesstokens=" + accesstokens + "order by time LIMIT %s"
-               # LIMIT %s"""%num
-        data= (num)
-        #query = ("SELECT * from Notifications where accesstokens='"+ username +"'order by time;")
-        #print(query)
-        cursor.execute(query,data)
-        print(query)
-        row_headers=[x[0] for x in cursor.description] #this will extract row headers
-        rv = cursor.fetchall()
-        json_data=[]
-        dict_results={}
+         )
 
-        for result in rv:
 
-          dict_results=dict(zip(row_headers,result))
-          print('dicresult',dict_results)
-          dict_results['sent']=dict_results['sent'].decode('utf-8')
+    device_data={access_hex:device}
 
-          dict_results['time']=str(dict_results['time'])
-          print('dicresult',dict_results)
-          if dict_results['sent'] =='\x00':
-            dict_results['sent']='False'
-          else:
-            dict_results['sent']='True'
-          json_data.append(dict_results)
-          #print(json_data)
+    db.child('devices').update(device_data)
 
-    cnx.close()
-    return (jsonify(json_data))
+    js = json.dumps(device_data)
+     # statusval=200
 
-@app.route('/recent')
-def getRecent():
+    # else:
+    #  print("users exist")
+    #  device_data={'User already exists':''}
+    # js = json.dumps(device_data)
+     # statusval=400
+
+    # resp = Response(js, status=statusval, mimetype='application/json')
+    # resp.headers['Link'] = 'http://127.0.0.1:5000/add_device?email='+email+'&device='+device
+    return(js)
+
+
+
+#no need to use
+@app.route('/getaccesstoken')
+def getaccesstoken():
     # When deployed to App Engine, the `GAE_ENV` environment variable will be
     # set to `standard`
-    if os.environ.get('GAE_ENV') == 'standard':
-        # If deployed, use the local socket interface for accessing Cloud SQL
-        unix_socket = '/cloudsql/{}'.format(db_connection_name)
-        cnx = pymysql.connect(user=db_user, password=db_password,
-                              unix_socket=unix_socket, db=db_name)
-    else:
-        # If running locally, use the TCP connections instead
-        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
-        # so that your application can use 127.0.0.1:3306 to connect to your
-        # Cloud SQL instance
-        host = '127.0.0.1'
-        host="34.125.50.185"
-        cnx = pymysql.connect(user=db_user, password=db_password,
-                              host=host, db=db_name)
-    accesstokens = request.args.get('accesstokens')
-    #number = request.args.get('n')
-    # print(username)
-    # print("num",number)
-    with cnx.cursor() as cursor:
 
-        # num=int(number)
-        # print(type(num))
-        query="SELECT * FROM Notifications where accesstokens=" + accesstokens + "and sent=0 order by time"
-               # LIMIT %s"""%num
-        #data= (num)
-        #query = ("SELECT * from Notifications where accesstokens='"+ username +"'order by time;")
-        #print(query)
-        cursor.execute(query)
-        print(query)
-        row_headers=[x[0] for x in cursor.description] #this will extract row headers
-        rv = cursor.fetchall()
-        json_data=[]
-        dict_results={}
+    email = request.args.get('email')
+    timestamp=datetime.utcnow()
+    #timestamp=time.time()
+    email_time=str(email)+str(timestamp)
+    #print(type(email_time))
+    hash_obj = hashlib.md5(email_time.encode())
+    return(jsonify(hash_obj.hexdigest()))
 
-        for result in rv:
 
-          dict_results=dict(zip(row_headers,result))
-          print('dicresult',dict_results)
-          dict_results['sent']=dict_results['sent'].decode('utf-8')
 
-          dict_results['time']=str(dict_results['time'])
-          print('dicresult',dict_results)
-          if dict_results['sent'] =='\x00':
-            dict_results['sent']='False'
-          else:
-            dict_results['sent']='True'
-          json_data.append(dict_results)
-          #print(json_data)
+def getnotifications(email_hex):
+       dict_accesstokens=db.child('users').child(email_hex).get().val() #get accesstokens, will have one accesstoken fro sure
+       print("dictvalues",dict_accesstokens.values())
+       accessVal_List=dict_accesstokens.values() #values of accesstokens, could be [0,0,{ts:{}}]
+       result_array=[]
 
-    cnx.close()
-    return (jsonify(json_data))
+       # for key, value in dict_accesstokens.items():
+       #  for item in accessVal_List: #item--> [0,0, {ts:{}}]
+       #    print("item",item)
+       #    if(item!=0):                           #timestamp values exists
+       #     for val in item.values():  #val--> {device:0, level:1, msgs:"warning"}
+       #       result_array.append(val)
 
-# [END gae_python37_cloudsql_mysql]
+       for access_hex, values in dict_accesstokens.items():
+           for item in values: #item--> [0,0, {ts:{}}]
+               print("access_token",access_hex)
+               if(item!=0):                           #timestamp values exists
+                  for val in item.values():  #val--> {device:0, level:1, msgs:"warning"}
+                    result_array.append(val)
+                    db.child('users').child(email_hex).update(
+                        {
+                          access_hex:0
+                        })
+
+
+       return(result_array) #no quotes email
+
+@app.route('/unread')
+def unread():
+    email = request.args.get('email')
+    email_obj = hashlib.md5(email.encode())
+    email_hex=email_obj.hexdigest()
+    if not db.child('users').child(email_hex).shallow().get().val(): #email doesnt exist
+      #db.setValue(hash_hex);
+        return ("User doesn't exist")
+    def event_stream():
+        while True:
+            # wait for source data to be available, then push it
+            yield 'data: {}\n\n'.format(getnotifications(email_hex))
+    return flask.Response(event_stream(), mimetype="text/event-stream")
+
 
 
 if __name__ == '__main__':
