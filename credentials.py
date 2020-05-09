c07c7aebb3edde9cdee2f276baae32a04599e811
asquare10 <55037129+asquare10@users.noreply.github.com>
2020-05-09 00:26:07 -0700

    Updated credentials.py

--- credentials.py
+++ credentials.py
@@ -4,7 +4,4 @@ def get_credentials():
     creds = filep.read().split(',')
     email = creds[0].split(':')[1][1:-1]
     accesstoken = creds[1].split(':')[1][1:-3]
-    print(email, accesstoken)
     return (email, accesstoken)
-
-get_credentials()
