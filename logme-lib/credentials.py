957df9cdd2bd4bbc42f71b89448e0c5ef9a091d6
asquare10 <55037129+asquare10@users.noreply.github.com>
2020-05-08 23:58:03 -0700

    Updated credentials.py

--- logme-lib/credentials.py
+++ logme-lib/credentials.py
@@ -1,7 +1,7 @@
 def get_credentials():
     file_path = "credentials/credentials.txt"
     filep = open(file_path, "r")
-    creds = filep.read().split("\n")
+    creds = filep.read().split(',')
     email = creds[0].split(':')[1]
-    accesstoken = creds[1].split(':')[1]
+    accesstoken = creds[1].split(':')[1][:-2]
     return (email, accesstoken)
